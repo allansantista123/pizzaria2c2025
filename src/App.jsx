eff834e7626dc36657e2800f286dc9e092495d87
allansantista123 <rm94425@estudante.fieb.edu.br>
2025-03-19 08:27:52 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ function App(){
  
   const [nome, setNome] = useState('Ronaldo')
  
-  return (
-    <div>
-      <h3>Pizzaria 2C</h3>
-      <input
+const Formulario = () => {
+
+   return (
+<div>
+   <input
         className="nome"
         onChange={(e)=>{setNome(e.target.value)}}
         name="nome"
@@ -17,6 +18,16 @@ function App(){
         className="botao">
         CLIQUE AQUI
       </button>
+</div>
+   )
+}
+
+  return (
+    <div>
+      <h3>Pizzaria 2C</h3>
+       <Formulario/>
+       <Formulario/>
+       <Formulario/>
     </div>
   )
 }
